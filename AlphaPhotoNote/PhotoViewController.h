201aafecc8c0cbf3cc62f853357631ace1fbf836
Juan Ribes <ribes03@yahoo.com>
2013-05-16 17:44:30 +0200

    PhotoAnnotation NO ADS 1.0

--- AlphaPhotoNote/PhotoViewController.h
+++ AlphaPhotoNote/PhotoViewController.h
@@ -10,11 +10,5 @@
 #import "RotableViewController.h"
 #import "SmoothedBIView.h"
 
-
-static NSString * const kAdMobID_IPhone = @"a151890d7860169";
-static NSString * const kAdMobID_IPad = @"a151890ee5b6254";
-// a151890d7860169 //iPhone
-// a151890ee5b6254 //iPad// Use AdMob's "Smart" size banners
-
 @interface PhotoViewController : RotableViewController
 @end
